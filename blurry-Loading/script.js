c744898280f3390fcf12133905739382664de906
nikhilxe <72465503+nikhilxe@users.noreply.github.com>
2022-09-20 21:45:51 +0530

    dank

--- blurry-Loading/script.js
+++ blurry-Loading/script.js
@@ -2,7 +2,7 @@ const loadText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
 
 
-let int = setInterval(blurring, 30);
+let int = setInterval(blurring, 50);
 let load = 0;
 function blurring() {
     load++;
@@ -12,7 +12,7 @@ function blurring() {
     loadText.innerText = `${load}%`
 
     loadText.style.opacity = scale(load, 0, 100, 1, 0);
-    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+    bg.style.filter = `blur(${scale(load, 0, 100, 50, 0)}px)`;
 }
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
